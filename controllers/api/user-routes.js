961bd5f2bb8e626533e6ffab879cffd3a94137e6
michael seaman <mseaman26@gmail.com>
2022-11-05 15:27:09 -0700

    set up a file structure and some minimal code to get the server to listen without errors

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try{
+
+    }catch(err){
+
+    }
+})
+
+module.exports = router;
