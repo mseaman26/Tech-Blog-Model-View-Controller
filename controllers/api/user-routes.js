4e4d42a42e4b7d0720220b5487e97ae37fee7241
michael seaman <mseaman26@gmail.com>
2022-11-06 00:06:05 -0700

    sign up form creates new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,7 +4,8 @@ const { User } = require('../../models');
 //create new user
 router.post('/', async (req, res) => {
     try{
-      const dbUserData = await User.create({
+        console.log(req.body.email)
+      const newUser = await User.create({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
@@ -13,13 +14,34 @@ router.post('/', async (req, res) => {
       req.session.save(() => {
         req.session.loggedIn = true
 
-        res.status(200).json(dbUserData)
+        res.status(200).json(newUser)
       })
     }catch(err){
         console.log(err)
         res.status(500).json(err)
     }
 
+})
+
+router.post('/login', async (req, res) => {
+    try{
+       
+        const currentUser = await User.findOne({
+            where: {
+                email: req.body.email
+            },
+        }) 
+        if (!currentUser){
+            res.status(404).json("Login Failed.  Incorrect username and/or password")
+            return
+        }
+        req.session.save(() => {
+            req.session.loggedIn = true
+            res.status(200).json.apply({message: "Login Successful!"})
+        })
+    }catch(err){
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -22,12 +22,12 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector('#email-signup').value.trim();
-    const email = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
     if (username && email && password) {
-      console.log("get here?")
+      console.log(email)
       const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
