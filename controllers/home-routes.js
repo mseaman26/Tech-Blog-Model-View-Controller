a234dae7faf90db0b9731953d21f019c3b3dbf53
michael seaman <mseaman26@gmail.com>
2022-11-05 19:22:59 -0700

    started user login functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,10 @@ const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
 
+router.get('/', (req, res) => {
+    res.render('homepage')
+})
+
 
 
 module.exports = router;
