65cca62b3cf8a6ff1796fda3be873de826e25de8
michael seaman <mseaman26@gmail.com>
2022-11-08 18:03:40 -0800

    getting closer to being able to edit a post

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,11 +1,19 @@
 
-
-
 const editPostHandler = async (event) => {
     event.preventDefault()
     const id = document.querySelector('.edit-post-title').id
-    
-    console.log(id)
+    const postToUpdate = await fetch(`/api/posts/${id}`, {
+      method: 'GET'
+    })
+    const title = "hello"
+    const body = "hello"
+    const updatedPost = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({title: title, body: body}),
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    console.log(updatedPost)
 }
 
 
